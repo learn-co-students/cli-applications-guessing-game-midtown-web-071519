725ed82d9e1455d7fd7a2aa4871db2639645c299
Alyssa Farina <alyssafarina695@gmail.com>
2019-07-10 23:16:54 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -12,6 +12,6 @@ def run_guessing_game
       puts "Guess a number between 1 and 6".\n
       number = rand(1..6).to_s
       input = gets.chomp
-    if input == "exit" 
+  end 
       puts "Goodbye!"
 end
